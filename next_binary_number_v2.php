0eb621c566e4e12228054266095a29ae0f891485
truffolone <dverra@gmail.com>
2017-01-16 19:07:11 +0100

    Added a second version using only one for cycle

---
+++ next_binary_number_v2.php
@@ -0,0 +1,33 @@
+<?php 
+
+function next_binary_number(array $input) {
+    $lastindex = count($input) - 1;
+    echo niceShow(addOne($input));
+}
+
+function addOne(array $arr) {
+    $lastindex = count($arr) - 1;
+    for($i = $lastindex; $i >= 0; $i--) {
+        if($arr[$i] == 0) {
+            $arr[$i] = 1;
+            return $arr;
+        } else {
+            $arr[$i] = 0;
+        }
+    }
+    //if we got here wi didn't get any 0
+    array_unshift($arr, 1);
+    return $arr;
+}
+
+function niceShow(array $array) {
+    $echo = "[";
+    for($i = 0; $i < count($array); $i++) {
+        if($i > 0) $echo .= ",";
+        $echo .= $array[$i];
+    }
+    $echo .= "]";
+    return $echo;
+}
+
+next_binary_number([1,1,0,1,0,1,1]);
